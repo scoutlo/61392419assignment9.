65e54533058012529eb7c33561b28b9d5a466cd2
scoutlo <112910014+scoutlo@users.noreply.github.com>
2022-12-09 20:09:12 -0600

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,94 @@
+/* Practice Assignment 9:
+
+Complete this javascript file according to the individual instructions
+given in the comments. 
+
+*** DO NOT CHANGE any of the code that you are not instructed to. */
+
+////////////////////////
+// NOTE: Use the API endpoints available at
+// https://jsonplaceholder.typicode.com/
+// to get the data required in these exercises.
+// HINT: Read the documented Resources and Routes.
+// Also the guide:
+// https://jsonplaceholder.typicode.com/guide/
+///////////////////////
+
+// 1) Create a function named "getAllPosts".
+// Use Fetch with Async/Await to request
+// all the posts. The function should
+// return all the posts as JSON data.
+
+let getAllpost = async()=> 
+{
+    const reply = await fetch("https://jsonplaceholder.typicode.com/post");
+    return await reply.json();
+}
+
+// 2) Create a function named "getAllUsers".
+// Use Fetch with Async/Await to request
+// all the posts. The function should
+// return all the posts as JSON data.
+
+let getAllUsers = async() =>
+{
+    const reply1 = await fetch ("https://jsonplaceholder.typicode.com/users")
+    return await reply1.json();
+}
+
+// 3) Create a function named "getComments".
+// This function needs to accept a "postID"
+// parameter.
+// Use Fetch with Async/Await to request
+// all the comments for the postID that
+// is passed in as a parameter.
+// The function should return all the
+// comments for the requested postID
+// as JSON data.
+
+let getComments = async(postID)=> 
+{
+    let arr = [];
+    const reply2 = await fetch ("https://jsonplaceholder.typicode.com/comments");
+    const data = awiat reply2.json();
+
+    data.map(comment) => 
+    {
+        if(comment.postID==postID)
+    }
+
+    return arr.push(comment);
+}
+
+
+// 4) Create a function named "getUser".
+// This function needs to accept a "userID"
+// parameter.
+// Use Fetch with Async/Await to request
+// the data for the requested user.
+// The function should return JSON data.
+
+const getUser = async(userID) => 
+{
+    const reply = await fetch("https://jsonplaceholder.typicode.com/" + userID);
+    const data = await reply.json();
+    return data;
+}
+
+// 5) Create a function named "getTodosForUser".
+// This function needs to accept a "userID"
+// parameter.
+// Use Fetch with Async/Await to request
+// all of the todos for the requested user.
+// The function should return JSON data.
+
+let getTodosForUser = async(userID) => 
+{
+    let arr = [];
+    const reply = await fetch("https://jsonplaceholder.typicode.com/todos?userid=$(userID)")
+    const a = await reply.json()
+   
+    a.map(todo) =>
+    {return arr.push(todo)}
+    return arr;
+}
